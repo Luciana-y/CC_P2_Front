7f8688d7b08196b895b6f49933a90d840317f698
Lucia Cartagena <lucia.cartagena@utec.edu.pe>
2025-11-22 19:29:11 -0500

    se modifico el vitevonfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // ← Esta es la carpeta de salida
+  },
 })
